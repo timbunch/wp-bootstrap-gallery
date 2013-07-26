cea1fc704e3d6958ef379dbb59079d8fe9265bb0
Tim Bunch <timbunch@gmail.com>
2013-07-26 14:01:23 -0600

    Switched from DIV to UL markup
    • Updated layouts
    • Switched from DIV layout to standard bootstrap UL
    • Added UL (row) breakpoints to fix padding issue

--- wp_bootstrap_gallery.php
+++ wp_bootstrap_gallery.php
@@ -10,6 +10,67 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+function bootstrap_gallery_shortcode($atts) {
+ 
+    global $post;
+ 
+    extract(shortcode_atts(array(
+        'orderby' => 'menu_order ASC, ID ASC',
+        'id' => $post->ID,
+        'itemtag' => 'dl',
+        'icontag' => 'dt',
+        'captiontag' => 'dd',
+        'columns' => 3,
+        'size' => 'medium',
+        'link' => 'file'
+    ), $atts));
+ 
+    $args = array(
+        'post_type' => 'attachment',
+        'post_parent' => $id,
+        'numberposts' => -1,
+        'orderby' => $orderby
+    ); 
+    $images = get_posts($args);
+    $image_count = count($images);
+    $heading = '<h1>'.$image_count.' Images</h1>';
+    $clearfix = '<div class="clearfix"></div>';
+    $ul_thumbnail = '<ul class="thumbnails">';
+    foreach ( $images as $image ) {     
+        $caption = $image->post_excerpt;
+ 
+        $description = $image->post_content;
+        if($description == '') $description = $title;
+
+        $image_alt = get_post_meta($image->ID,'_wp_attachment_image_alt', true);
+ 
+        $img = wp_get_attachment_image_src($image->ID, $size);
+        
+        // render your gallery here
+        $li_open = '<li class="span4">';
+        $image_html = "<img src='$img[0]' alt='$image_alt' />";
+        $li_close = '</li>';
+        
+        $img_complete = $li_open.$image_html.$li_close;
+        $image_set = $image_set.$img_complete;
+        
+    }
+    $ul_close = '</ul>';
+    
+    // RETURN THE RESULTS
+    return $clearfix.$heading.$ul_thumbnail.$image_set.$ul_close;
+}
+
+/**
+ * Class Name: wp_bootstrap_gallery
+ * GitHub URI: https://github.com/twittem/wp-bootstrap-gallery
+ * Description: A custom Wordpress gallery for dynamic thumbnail layout using Twitter Bootstrap 2 thumbnail layouts.
+ * Version: 1.0
+ * Author: Edward McIntyre - @twittem
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
 function wp_bootstrap_gallery( $content, $attr ) {
 	global $instance, $post;
 	$instance++;
@@ -40,9 +101,9 @@ function wp_bootstrap_gallery( $content, $attr ) {
 	}
 
 	if ( $include ) {
-		
+
 		$include = preg_replace( '/[^0-9,]+/', '', $include );
-		
+
 		$_attachments = get_posts( array(
 			'include'			=>	$include,
 			'post_status'		=>	'inherit',
@@ -53,15 +114,15 @@ function wp_bootstrap_gallery( $content, $attr ) {
 		) );
 
 		$attachments = array();
-		
+
 		foreach ( $_attachments as $key => $val ) {
 			$attachments[$val->ID] = $_attachments[$key];
 		}
 
 	} elseif ( $exclude ) {
-		
+
 		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
-		
+
 		$attachments = get_children( array(
 			'post_parent'		=>	$id,
 			'exclude'			=>	$exclude,
@@ -73,7 +134,7 @@ function wp_bootstrap_gallery( $content, $attr ) {
 		) );
 
 	} else {
-		
+
 		$attachments = get_children( array(
 			'post_parent'		=>	$id,
 			'post_status'		=>	'inherit',
@@ -100,10 +161,10 @@ function wp_bootstrap_gallery( $content, $attr ) {
 	$captiontag	=	tag_escape( $captiontag );
 	$columns	=	intval( min( array( 8, $columns ) ) );
 	$float		=	(is_rtl()) ? 'right' : 'left';
-	
+
 	$selector	=	"gallery-{$instance}";
 	$size_class	=	sanitize_html_class( $size );
-	$output		=	"<div class='row' id='$selector'>";
+	$output		=	"<ul class='thumbnails' id='$selector'>";
 
 	/**
  	 * Count number of items in $attachments array, and assign a colum layout to $span_array
@@ -115,55 +176,68 @@ function wp_bootstrap_gallery( $content, $attr ) {
 		case 1:
 			/* One full width image */
 	        $span_array = array(12);
+            $row_break = 0;
 	        break;
 	    case 2:
 	    	/* Two half width images */
 	        $span_array = array(6,6);
+            $row_break = 0;
 	        break;
 	    case 3:
 	    	/* One 3/4 width image with two 1/4 width images to the right */
 	        $span_array = array(8,4,4);
+            $row_break = 0;
 	        break;
 	    case 4:
 	    	/* One full width image with three 1/3 width images underneath */
-	    	$span_array = array(12,4,4,4);
+	    	$span_array = array(3,3,3,3);
+            $row_break = 0;
 	        break;
 	    case 5:
 	    	/* Two half width images with fout 1/4 width images underneath */
-	    	$span_array = array(6,6,4,4,4);
+	    	$span_array = array(4,4,4,6,6);
+            $row_break = 3;
 	        break;
 	    case 6:
 	    	/* One 2/3 width image with two 1/3 width images to the right,
 	    	 * and three 1/3 width images underneath */
 	    	$span_array = array(8,4,4,4,4,4);
+            $row_break = 3;
 	        break;
 	    default:
 	    	/* One full width image with two 1/2 width images underneath
 	    	 * All remaining images 1/3 width underneath */
-	    	$span_array = array(12,6,6,4);
+	    	$span_array = array(3,3,3,3);
+            $row_break = 4;
 	        break;
 	}
 
 	$attachment_count = 0;
 
 	foreach ( $attachments as $id => $attachment ) {
-		
+        if ($row_break and ( $row_break == $attachment_count ) ) {
+            $output .="</ul><ul class='thumbnails'>";
+            $row_break = $row_break+$row_break;
+        }
+        
 		$attachment_image = wp_get_attachment_image( $id, 'full');
 		$attachment_link = wp_get_attachment_link( $id, 'full', ! ( isset( $attr['link'] ) AND 'file' == $attr['link'] ) );
-		
-		$output .= "<div class='span" . $span_array[$attachment_count] . "'>";
-		$output .= $attachment_link . "\n";
-		$output .= "</div>\n";
 
-		if(count($attachments) >= 7 && $attachment_count == 3){
+		$output .= "<li class='span" . $span_array[$attachment_count] . "'>";
+		$output .= $attachment_link . "\n";
+		$output .= "</li>\n";
+
+		/*if(count($attachments) >= 7 && $attachment_count == 3){
 			$attachment_count = 3;
-		} else {
-			$attachment_count++;
-		}
+		} else {*/
+        $attachment_count++;
+		//}
+        
+            
 	}
-	
-	$output .= "</div>\n";
-	
+
+	$output .= "</ul>\n";
+
 	return $output;
 }
 
